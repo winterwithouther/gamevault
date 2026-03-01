3dd9c5781d44a2aaf8caf63c06e15431125c8606
Eman Gurung <egurung1117@gmail.com>
2026-03-01 10:53:49 -0800

    fetching single game data /:slug route, and rendering on front end

--- client/public/scripts/games.js
+++ client/public/scripts/games.js
@@ -1,8 +1,27 @@
 const gamesContainer = document.createElement('div')
 gamesContainer.className = 'games-container'
 
+async function renderPage() {
+    const path = window.location.pathname
+
+    // render all games
+    if (path === '/') {
+        renderGames()
+        return
+    }
+
+    // render single game route
+    if (path.startsWith('/games/')) {
+        const slug = path.split('/games/')[1]
+        renderSingleGame(slug)
+        return
+    }
+
+    app.textContent = 'Page not found'
+}
+
 async function renderGames() {
-    const response = await fetch('/games');
+    const response = await fetch('http://localhost:3000/api/games');
     if (!response.ok) throw new Error('Network response was not ok.')
 
     const games = await response.json()
@@ -39,6 +58,37 @@ async function renderGames() {
     })
 }
 
+async function renderSingleGame(slug) {
+    const response = await fetch(`http://localhost:3000/api/games/${slug}`)
+
+    if (!response.ok) {
+        app.textContent = 'Game not found'
+        return
+    }
+
+    const game = await response.json()
+
+    console.log(game)
+
+    const container = document.createElement('div')
+    container.className = 'single-game-container'
+
+    const title = document.createElement('h1')
+    title.textContent = game.title
+
+    const info = document.createElement('p')
+    info.textContent = `${game.genre} | Released: ${game.release}`
+
+    const dev = document.createElement('p')
+    dev.textContent = `Developer: ${game.developer}`
+
+    const desc = document.createElement('p')
+    desc.textContent = game.description
+
+    container.append(title, info, dev, desc)
+    document.getElementById('app').append(container)
+}
+
 document.getElementById('app').append(gamesContainer)
 
-renderGames()
+renderPage()
